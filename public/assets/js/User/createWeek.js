32db07a787ec9fb0a1d1a357becbaacc69733708
herivola <rchristinella@gmail.com>
2024-10-03 02:22:43 +0200

    Creer sa semaine utilisateur

--- public/assets/js/User/createWeek.js
+++ public/assets/js/User/createWeek.js
@@ -23,28 +23,23 @@ document.addEventListener('DOMContentLoaded', function() {
     activityForm.addEventListener('submit', function(event) {
         event.preventDefault(); 
 
-        // Récupération des éléments de sélection
         const sportSelect = document.getElementById('ID_sport');
         const programSelect = document.getElementById('ID_program');
         const exerciseSelect = document.getElementById('ID_Exercise');
 
-        // Récupération des valeurs sélectionnées
-        const sport = sportSelect.options[sportSelect.selectedIndex].text; // Récupère le texte de l'option sélectionnée
-        const program = programSelect.options[programSelect.selectedIndex].text; // Récupère le texte de l'option sélectionnée
-        const exercise = exerciseSelect.options[exerciseSelect.selectedIndex].text; // Récupère le texte de l'option sélectionnée
+        const sport = sportSelect.options[sportSelect.selectedIndex].text; 
+        const program = programSelect.options[programSelect.selectedIndex].text; 
+        const exercise = exerciseSelect.options[exerciseSelect.selectedIndex].text; 
         const day = dayInput.value;
 
-        // Créer une chaîne avec les informations
         const message = `Vous avez enregistré:\nJour: ${day}\nSport: ${sport}\nProgramme: ${program}\nExercice: ${exercise}`;
 
-        // Afficher les informations
         alert(message);
 
-        // Optionnel: Vous pouvez également masquer la modale après l'enregistrement
         document.getElementById('modal').style.display = 'none';
     });
 
-    // Gestionnaire pour fermer la modale
+
     document.querySelector('.close-button').addEventListener('click', function() {
         document.getElementById('modal').style.display = 'none';
     });
